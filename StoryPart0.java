efb102410fa2b413a449818285dd62c879a3276c
joanna <joanna>
2017-01-12 20:44:14 -0500

    protected

--- StoryPart0.java
+++ StoryPart0.java
@@ -1,5 +1,5 @@
 public class StoryPart0 extends StoryPart {
-    Clue planner;
+    protected Clue planner;
     public StoryPart0() {
 	super();
 	planner = new Clue("Planner", "Says 'Meet Caro' day before disappearence and 'Shake down customers' the day of. Weird.");
