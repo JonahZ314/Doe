f2ed47268c43a4ead701dd9b83d02156d5418b2f
JonahZ314 <jzhou7@stuy.edu>
2017-01-15 16:06:08 -0500

    Minor spelling edits

--- Woo.java
+++ Woo.java
@@ -58,7 +58,7 @@ public class Woo {
 	if (a == 2) {
 	    System.out.println("Congratulations Sherlock! You've solved the crime!"); 
 	} else {
-	    System.out.println("You point your finger in confidence, but unfortunately, you have mistaken. With your incorrect accusation, an innocent has been made to suffer unjust consequences." + "\nThe End");
+	    System.out.println("You point your finger in confidence, but unfortunately, you were mistaken. With your incorrect accusation, an innocent has been made to suffer unjust consequences." + "\nThe End");
 	}
     }
 
